cb5392f4a9e09b9d88d9f2416954369c8f0be925
Wali <walikhan226@gmail.com>
2021-05-10 05:27:15 +0500

    Initial Commit

---
+++ firstapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstapp
+//
+//  Created by Mapple technologies on 10/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by Mapple technologies on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
